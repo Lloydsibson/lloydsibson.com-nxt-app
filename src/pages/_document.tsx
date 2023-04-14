799da594673c422ab2abe901d199a082892e8cc8
Lloyd Sibson <lloydsibson@gmail.com>
2023-04-14 13:05:18 +0100

    Dev: Added GTM tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,30 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* <!-- Google Tag Manager --> */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PZP5NRH');`,
+          }}
+        />
+      </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PZP5NRH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        ></noscript>
         <div className="website-background"></div>
         <noscript>You need to enable JavaScript to run this app.</noscript>
         <Main />
