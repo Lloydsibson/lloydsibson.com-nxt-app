41f852d268c4000c1f493e4d24984b0edfa9cdff
Lloyd Sibson <lloydsibson@Lloyds-Mac-Studio.local>
2023-07-09 13:06:05 +0100

    Dev: HTML Video resolved for Safari

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -1,41 +1,43 @@
 import { useEffect, useState, useRef } from "react";
 
 const VideoBackground = () => {
-  const [backgroundVideoURL, setBackgroundVideoURL] = useState<string>("");
-  const ref = useRef<any>(null);
+  // const [backgroundVideoURL, setBackgroundVideoURL] = useState<string>("");
+  // const ref = useRef<any>(null);
 
-  useEffect(() => {
-    // LOADS IN VIDEO AFTER PAGE HAS LOADED - IMPROVING PAGE PERFORMANCE
-    setTimeout(function () {
-      setBackgroundVideoURL(
-        "https://res.cloudinary.com/cloudinary-ls-images/video/upload/q_auto:eco/v1610808503/img/Sunset-hb-vimeo-2k-web.mp4"
-      );
-      const videoElem = ref.current;
-      videoElem.load();
-      const playPromise = videoElem.play();
-      if (playPromise !== null) {
-        playPromise.catch(() => {
-          videoElem.play();
-        });
-      }
-    }, 250);
-  }, []);
+  // useEffect(() => {
+  //   // LOADS IN VIDEO AFTER PAGE HAS LOADED - IMPROVING PAGE PERFORMANCE
+  //   setTimeout(function () {
+  //     setBackgroundVideoURL(
+  //       "https://res.cloudinary.com/cloudinary-ls-images/video/upload/q_auto:eco/v1610808503/img/Sunset-hb-vimeo-2k-web.mp4"
+  //     );
+  //     const videoElem = ref.current;
+  //     videoElem.load();
+  //     const playPromise = videoElem.play();
+  //     if (playPromise !== null) {
+  //       playPromise.catch(() => {
+  //         videoElem.play();
+  //       });
+  //     }
+  //   }, 250);
+  // }, []);
   return (
     <section className="html-video-container">
       <video
         autoPlay
-        ref={ref}
+        //ref={ref}
         loop
         muted
         playsInline
         // ref="video"
-        controls={false}
         className="videoplayer"
         id="video"
         controlsList="nodownload"
         poster="https://res.cloudinary.com/cloudinary-ls-images/image/upload/v1610816567/img/video-placeholder.png"
       >
-        <source type="video/mp4" src={backgroundVideoURL} />
+        <source
+          type="video/mp4"
+          src="https://res.cloudinary.com/cloudinary-ls-images/video/upload/q_auto:eco/v1610808503/img/Sunset-hb-vimeo-2k-web.mp4"
+        />
       </video>
       {/* <img
         className="html-video-container__img"
