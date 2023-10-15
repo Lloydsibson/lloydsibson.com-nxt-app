9d97ba5b646ab8080de8867006dc143d59ea5d92
Lloyd Sibson <lloydsibson@Lloyds-Mac-Studio.local>
2023-10-15 11:24:11 +0100

    Dev: Cloudflare Font tweak

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,7 +33,7 @@ f=false,w=window,d=document,v=d.querySelector('#vwoCode'),cK='_vwo_'+account_id+
         {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" /> */}
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
           rel="stylesheet"
         />
         <meta charSet="utf-8" />
