de5cd76649bdf94e03be6ed5573087b73bdd5ca2
Lloyd Sibson <lloydsibson@Lloyds-Macbook-Pro-14-M3-Pro.local>
2024-03-03 11:30:31 +0000

    Dev: improved accessibility for screen readers

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -7,9 +7,20 @@ export const BackToTop = () => {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   };
 
+  const BackToTopScrollEnterKey = (e: any) => {
+    if (e.keyCode === 13) {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+  };
+
   return (
-    <div className="bk-to-top-container" onClick={BackToTopScroll}>
-      <div className="bk-to-top-container__icon">
+    <div
+      className="bk-to-top-container"
+      onClick={BackToTopScroll}
+      onKeyDown={BackToTopScrollEnterKey}
+    >
+      <div className="bk-to-top-container__icon" tabIndex={0}>
         <FontAwesomeIcon icon={faChevronUp} />
       </div>
     </div>
