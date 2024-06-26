fb5eb2214f948b6a4cfd4ba0bad68a126238c4d9
Lloyd Sibson <lloydsibson@gmail.com>
2024-06-26 08:40:14 +0100

    Dev: Set Sentry to working ONLY in PROD environments - due to increasing reloads in dev

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -7,7 +7,7 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://29b6389b1585479cbcd08fcf6e16e9cb@o259988.ingest.sentry.io/4505501068558336",
-
+  enabled: process.env.NODE_ENV === "production",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
