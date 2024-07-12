7e80fd8220309dae80f89ba320fd65ed1df22642
Lloyd Sibson <lloydsibson@gmail.com>
2024-07-13 00:50:27 +0100

    Dev: Updated Video Component + added Github Action for Lighthouse Reporting

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -1,45 +1,51 @@
-import React, { useState, useRef, useEffect } from "react";
+// import { useEffect, useState, useRef } from "react";
 
 const VideoBackground = () => {
-  const [backgroundVideoURL, setBackgroundVideoURL] = useState<string>("");
-  const ref = useRef<HTMLVideoElement>(null);
+  // const [backgroundVideoURL, setBackgroundVideoURL] = useState<string>("");
+  // const ref = useRef<any>(null);
 
-  useEffect(() => {
-    // LOADS IN VIDEO AFTER PAGE HAS LOADED - IMPROVING PAGE PERFORMANCE
-    setTimeout(function () {
-      setBackgroundVideoURL(
-        "https://res.cloudinary.com/cloudinary-ls-images/video/upload/q_auto:eco/v1610808503/img/Sunset-hb-vimeo-2k-web.mp4"
-      );
-      const videoElem = ref.current;
-      if (videoElem) {
-        videoElem.load();
-        const playPromise = videoElem.play();
-        if (playPromise !== undefined) {
-          playPromise.catch(() => {
-            videoElem.play();
-          });
-        }
-      }
-    }, 250);
-  }, []);
-
+  // useEffect(() => {
+  //   // LOADS IN VIDEO AFTER PAGE HAS LOADED - IMPROVING PAGE PERFORMANCE
+  //   setTimeout(function () {
+  //     setBackgroundVideoURL(
+  //       "https://res.cloudinary.com/cloudinary-ls-images/video/upload/q_auto:eco/v1610808503/img/Sunset-hb-vimeo-2k-web.mp4"
+  //     );
+  //     const videoElem = ref.current;
+  //     videoElem.load();
+  //     const playPromise = videoElem.play();
+  //     if (playPromise !== null) {
+  //       playPromise.catch(() => {
+  //         videoElem.play();
+  //       });
+  //     }
+  //   }, 250);
+  // }, []);
   return (
     <section className="html-video-container">
       <video
         autoPlay
-        ref={ref}
+        //ref={ref}
         loop
         muted={true}
         playsInline
+        // ref="video"
         className="videoplayer"
         id="video"
         controlsList="nodownload"
         poster="https://res.cloudinary.com/cloudinary-ls-images/image/upload/f_auto,q_auto,e_blur:100/v1610816567/img/video-placeholder"
       >
-        {backgroundVideoURL && (
-          <source type="video/mp4" src={backgroundVideoURL} />
-        )}
+        <source
+          type="video/mp4"
+          src="https://res.cloudinary.com/cloudinary-ls-images/video/upload/q_auto:eco/v1610808503/img/Sunset-hb-vimeo-2k-web.mp4"
+        />
       </video>
+      {/* <img
+        className="html-video-container__img"
+        src="https://res.cloudinary.com/cloudinary-ls-images/image/upload/v1610816567/img/video-placeholder.png"
+        alt="Man with his back turned"
+      /> */}
+
+      {/* <div className="mask"></div> */}
     </section>
   );
 };
