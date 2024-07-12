17cd6070e8a9fd72d0b5e46f0eca6ad7ecb00a1c
Lloyd Sibson <lloydsibson@gmail.com>
2024-07-12 14:16:36 +0100

    Dev: Lighthouse Automation - Added the ability to report JSON too - Commented the code out for now as it will increase build times

--- nodeLightHouseReport.js
+++ nodeLightHouseReport.js
@@ -11,20 +11,34 @@ const formFactors = ["mobile", "desktop"];
 
 urls.forEach((url) => {
   formFactors.forEach((formFactor) => {
-    const filename = `${url
+    const baseFilename = `${url
       .split("//")[1]
-      .replace(/\W+/g, "_")}_${formFactor}.html`;
-    // Include the --headless flag in the options
-    const options = `--output=html --output-path=public/lighthouse-reports/${filename} --emulated-form-factor=${formFactor} --chrome-flags="--headless"`;
+      .replace(/\W+/g, "_")}_${formFactor}`;
+    const htmlFilename = `public/lighthouse-reports/${baseFilename}.html`;
+    //const jsonFilename = `lighthouse-json/${baseFilename}.json`; // Specify a different location for JSON reports
 
-    exec(`lighthouse ${url} ${options}`, (error, stdout, stderr) => {
+    // Command for HTML report
+    const htmlOptions = `--output=html --output-path=${htmlFilename} --emulated-form-factor=${formFactor} --chrome-flags="--headless"`;
+    exec(`lighthouse ${url} ${htmlOptions}`, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
         return;
       }
       console.log(
-        `Lighthouse report for ${url} (${formFactor}) generated at ./lighthouse/${filename}.`
+        `HTML Lighthouse report for ${url} (${formFactor}) generated at ./${htmlFilename}.`
       );
     });
+
+    // Command for JSON report
+    // const jsonOptions = `--output=json --output-path=${jsonFilename} --emulated-form-factor=${formFactor} --chrome-flags="--headless"`;
+    // exec(`lighthouse ${url} ${jsonOptions}`, (error, stdout, stderr) => {
+    //   if (error) {
+    //     console.error(`exec error: ${error}`);
+    //     return;
+    //   }
+    //   console.log(
+    //     `JSON Lighthouse report for ${url} (${formFactor}) generated at ./${jsonFilename}.`
+    //   );
+    // });
   });
 });
