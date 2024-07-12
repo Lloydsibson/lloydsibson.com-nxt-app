fc0165db77d955c9c2b7e9f6f814e357eda82b23
Lloyd Sibson <lloydsibson@gmail.com>
2024-07-12 13:41:01 +0100

    Dev: Updated Lighthouse Report Automation to be headless, added desktop and mobile reporting and hid these new URL from google search

--- nodeLightHouseReport.js
+++ nodeLightHouseReport.js
@@ -7,18 +7,24 @@ const urls = [
   "https://www.lloydsibson.com/contact",
 ];
 
-urls.forEach((url) => {
-  // Generate a unique filename for each URL's report
-  const filename = url.split("//")[1].replace(/\W+/g, "_") + ".html";
-  const options = `--output=html --output-path=public/lighthouse-reports/${filename}`;
+const formFactors = ["mobile", "desktop"];
 
-  exec(`lighthouse ${url} ${options}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(
-      `Lighthouse report for ${url} generated at ./lighthouse/${filename}.`
-    );
+urls.forEach((url) => {
+  formFactors.forEach((formFactor) => {
+    const filename = `${url
+      .split("//")[1]
+      .replace(/\W+/g, "_")}_${formFactor}.html`;
+    // Include the --headless flag in the options
+    const options = `--output=html --output-path=public/lighthouse-reports/${filename} --emulated-form-factor=${formFactor} --chrome-flags="--headless"`;
+
+    exec(`lighthouse ${url} ${options}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(
+        `Lighthouse report for ${url} (${formFactor}) generated at ./lighthouse/${filename}.`
+      );
+    });
   });
 });
