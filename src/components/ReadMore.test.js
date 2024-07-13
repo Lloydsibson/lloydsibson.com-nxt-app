21f9675c2a81ee9ade72c0377b0cf7189c5f8fdf
Lloyd Sibson <lloydsibson@gmail.com>
2024-07-13 22:02:06 +0100

    Dev: Created a Unit Test

---
+++ src/components/ReadMore.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { ReadMoreBtn } from "./ReadMore"; // Adjust the import path as necessary
+
+describe("ReadMoreBtn", () => {
+  it("renders with provided text", () => {
+    render(<ReadMoreBtn text="Read More" />);
+    expect(screen.getByTestId("rm-button-text")).toHaveTextContent("Read More");
+  });
+});
