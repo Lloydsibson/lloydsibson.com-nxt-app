858734db9cdd1b075a032cd364f9accbe1a325fe
Lloyd Sibson <lloydsibson@gmail.com>
2024-07-14 10:38:12 +0100

    Dev: Testing updated CI/CD workflow

--- .github/workflows/lighthouse-config.js
+++ .github/workflows/lighthouse-config.js
@@ -1,20 +1,20 @@
-module.exports = {
-  extends: "lighthouse:default",
-  emulatedUserAgent: false,
-  settings: {
-    formFactor: "desktop",
-    screenEmulation: {
-      width: 1920,
-      height: 1080,
-      deviceScaleRatio: 1,
-      mobile: false,
-      disabled: false,
-    },
-    audits: [
-      {
-        path: "metrics/accessibility",
-        options: { score: 0.98 },
-      },
-    ],
-  },
-};
+// module.exports = {
+//   extends: "lighthouse:default",
+//   emulatedUserAgent: false,
+//   settings: {
+//     formFactor: "desktop",
+//     screenEmulation: {
+//       width: 1920,
+//       height: 1080,
+//       deviceScaleRatio: 1,
+//       mobile: false,
+//       disabled: false,
+//     },
+//     audits: [
+//       {
+//         path: "metrics/accessibility",
+//         options: { score: 0.98 },
+//       },
+//     ],
+//   },
+// };
