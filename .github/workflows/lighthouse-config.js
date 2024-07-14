4ae4055d3bf6c5880401fe3b207bd2e4dd33396f
Lloyd Sibson <lloydsibson@gmail.com>
2024-07-14 02:21:03 +0100

    Dev: Updated Github Action

--- .github/workflows/lighthouse-config.js
+++ .github/workflows/lighthouse-config.js
@@ -1,16 +1,18 @@
 module.exports = {
   extends: "lighthouse:default",
+  formFactor: "desktop",
   settings: {
     screenEmulation: {
       width: 1920,
       height: 1080,
+      deviceScaleRatio: 1,
       mobile: false,
       disabled: true,
     },
     audits: [
       {
-        path: "metrics/first-contentful-paint",
-        options: { scorePODR: 800, scoreMedian: 1600 },
+        path: "metrics/accessibility",
+        options: { score: 0.9 },
       },
     ],
   },
