ca1f910d35026e632c6c84e1056ebea3cf57eb3c
Lloyd Sibson <lloydsibson@gmail.com>
2025-05-19 08:50:33 +0100

    Dev: Profile photo update

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -164,9 +164,9 @@ const AboutPage = () => {
               }}
             ></div> */}
             <AdvancedLazyLoad
-              imgURL="https://res.cloudinary.com/cloudinary-ls-images/image/upload/f_auto,q_auto/v1667817743/img/linkedin-office-photo-tp_jjfyuo"
+              imgURL="https://res.cloudinary.com/cloudinary-ls-images/image/upload/v1747640740/profile-photo-25_pdlzda"
               imgALT="Lloyd Sibson in office"
-              backgroundImg="https://res.cloudinary.com/cloudinary-ls-images/image/upload/w_80/e_blur:300,f_auto,q_auto/v1667817743/img/linkedin-office-photo-tp_jjfyuo"
+              backgroundImg="https://res.cloudinary.com/cloudinary-ls-images/image/upload/w_80/e_blur:300,f_auto,q_auto/v1747640740/profile-photo-25_pdlzda"
               imgIdName={undefined}
               elemName="profile-img-container"
               imgTransparent={false}
@@ -1008,7 +1008,7 @@ const AboutPage = () => {
               <FontAwesomeIcon icon={faGamepad} />
               <h3>Recently Played</h3>
               {/* <span>{latestGamePlayed ? latestGamePlayed : "N/A"}</span> */}
-              <span>Astro Bot</span>
+              <span>DOOM: Dark Age</span>
             </div>
             <div className="fun-facts-container__fun-facts">
               <FontAwesomeIcon icon={faDumbbell} />
