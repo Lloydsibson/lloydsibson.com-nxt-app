b344d14a4a948391470a3817a1e2e00df85d9f31
Lloyd Sibson <lloydsibson@gmail.com>
2025-05-19 09:00:57 +0100

    Dev: Added new portfolio example

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -59,12 +59,19 @@ const PortfolioPage = () => {
   }, []);
   const LinkData = [
     {
-      url: "https://www.figma.com/design/iGYZapLn1bivJyg84Nz2vn/Portfolio-Page---Links---Pop-up?node-id=0-1&t=J25vyDEXzjkqOxh7-1",
-      linkText: "Links Pop-Up",
-      postedDate: new Date(2024, 5, 28),
+      url: "https://www.figma.com/design/pmXiOPl70TX3BC5m0lFFSp/Falling-Below---Website?node-id=0-1&t=OodygTvqRjF5uXve-1",
+      linkText: "Website Re-Design",
+      postedDate: new Date(2025, 4, 19),
       icon: faFigma,
-      tags: ["UI", "Share", "Likes"],
+      tags: ["Falling Below", "UI", ""],
     },
+    // {
+    //   url: "https://www.figma.com/design/iGYZapLn1bivJyg84Nz2vn/Portfolio-Page---Links---Pop-up?node-id=0-1&t=J25vyDEXzjkqOxh7-1",
+    //   linkText: "Links Pop-Up",
+    //   postedDate: new Date(2024, 5, 28),
+    //   icon: faFigma,
+    //   tags: ["UI", "Share", "Likes"],
+    // },
     {
       url: "https://codepen.io/lloydsibson/pen/abrwgaq",
       linkText: "VIP Slider",
