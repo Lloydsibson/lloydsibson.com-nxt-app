a8abcf78cdd9f440f18983458c9b105b83dffe50
Lloyd Sibson <lloydsibson@gmail.com>
2025-10-06 11:30:06 +0100

    Dev: Updated job title

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -198,7 +198,7 @@ const AboutPage = () => {
             </div>
 
             <h3 className="bio-info-container__sub-heading">
-              Front End Developer (CRO) @ <span>British Gas</span> ⚡ | A/B
+              CRO & Front End Developer @ <span>British Gas</span> ⚡ | A/B
               Testing | Adobe Target | React | JavaScript | CSS | HTML | UI/UX
             </h3>
             <p className="bio-info-container__country">
